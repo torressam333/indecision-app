04d83d3e26312b6f3dae3f7f2fe9b337d433b993
Samuel Torres <torressam333@gmail.com>
2019-09-30 19:22:25 -0700

    Initial Repo Commit

---
+++ public/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// JSX - JavaScript XML
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Indecision Application'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  )
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,5 @@
+// JSX - JavaScript XML
+var template = <div><h1>Indecision Application</h1><p>This is some info</p></div>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
