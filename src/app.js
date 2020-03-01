3d3a9657275aed02f955510342ab78e0c46def0e
Samuel Torres <torressam333@gmail.com>
2020-03-01 16:57:04 -0700

    Refactor setState portion of components to use less code

--- src/app.js
+++ src/app.js
@@ -9,12 +9,8 @@ class IndecisionApp extends React.Component {
         };
     }
 
-    handleDeleteOptions() {
-        this.setState(() => {
-            return {
-                options: []
-            };
-        });
+    handleDeleteOptions () {
+        this.setState(() => ({options: []}));
     }
 
     handlePick() {
@@ -30,11 +26,9 @@ class IndecisionApp extends React.Component {
             return 'This option already exists';
         }
 
-        this.setState((prevState) => {
-            return {
-                options: prevState.options.concat(option)
-            };
-        });
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option)
+        }));
     }
 
     render() {
@@ -125,9 +119,7 @@ class AddOption extends React.Component {
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
 
-        this.setState(() => {
-            return {error};
-        });
+        this.setState(() => ({error}));
     }
 
     render() {
