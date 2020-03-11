d080f04efb9d25ea6d5841aacb5f86439cfa3feb
Samuel Torres <torressam333@gmail.com>
2020-03-10 18:56:21 -0700

    Add lifecycle methods to counter-example.js +Persist count with Local Storage

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -8,6 +8,30 @@ class Counter extends React.Component {
             count: 0
         };
     }
+
+    //Lifecycle Methods
+    componentDidMount(){
+        //Use try/catch for invalid json
+        try{
+            const json = localStorage.getItem('count');
+            const count = JSON.parse(json);
+
+            if (count) {
+                //Use state to return an object
+                this.setState(() => ({count}));
+            }
+        }catch (e) {
+            //Do nothing with invalid JSON, allow to be default state value
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        //check if count changed by comparing to current state count
+        if (prevState.count !== this.state.count) {
+            const json = JSON.stringify(this.state.count);
+            localStorage.setItem('count', json);
+        }
+    }
     handleAddOne() {
         this.setState((prevState) => {
             return {
