4286c68706ffd437e1acfa64ee6d3b65a546e1e5
Samuel Torres <torressam333@gmail.com>
2020-03-16 15:25:02 -0700

    Introduce webpack to application

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+//Entry point of our app
+module.exports = {
+  //config details for webpack build
+    entry: './src/app.js',
+    //Where to output bundle.js
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+};
