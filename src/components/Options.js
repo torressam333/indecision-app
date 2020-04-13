0e21ac0f95b93f9c5698a8e0dde7a715d551fbfd
Samuel Torres <torressam333@gmail.com>
2020-04-13 10:25:27 -0700

    Style the widget header

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,23 +1,28 @@
 import React from "react";
 import Option from "./Option";
 
-const Options = (props) =>  (
-        <div>
+const Options = (props) => (
+    <div>
+        <div className="widget-header">
+            <h3 className="widget-header__h3-header" >Your Options</h3>
             <button
                 className="button button--link"
-                onClick={props.handleDeleteOptions}>Remove All</button>
-            {/*Add message if options array is empty*/}
-            {props.options.length === 0 && <p>Please add an option to get started</p>}
-            {
-                props.options.map((option) => (
-                    <Option
-                        key={option}
-                        optionText={option}
-                        handleDeleteOption={props.handleDeleteOption}
-                    />
-                ))
-            }
+                onClick={props.handleDeleteOptions}>
+                Remove All
+            </button>
         </div>
-    );
+        {/*Add message if options array is empty*/}
+        {props.options.length === 0 && <p>Please add an option to get started</p>}
+        {
+            props.options.map((option) => (
+                <Option
+                    key={option}
+                    optionText={option}
+                    handleDeleteOption={props.handleDeleteOption}
+                />
+            ))
+        }
+    </div>
+);
 
 export default Options;
