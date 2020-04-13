3a59d0b5c7805fbfdb2156d43bee2502e398fea6
Samuel Torres <torressam333@gmail.com>
2020-04-13 10:53:41 -0700

    Style the individual option text

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 const Option = (props) => (
-        <div>
-            {props.optionText}
-            {/*Delete individual option*/}
-            <button
-                className="button button--link"
-                onClick={(e) => {
-                    props.handleDeleteOption(props.optionText);
-                }}
-            >
-                Remove Option
-            </button>
-        </div>
-    );
+    <div className='option'>
+        <p className= "option__text">{props.count}. {props.optionText} </p>
+        {/*Delete individual option*/}
+        <button
+            className="button button--link"
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText);
+            }}
+        >
+            Remove Option
+        </button>
+    </div>
+);
 
 export default Option;
